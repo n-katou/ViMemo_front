ed4be76de73e4c7faf39a74f2c588777e0e5ebac
n-katou <102382177+n-katou@users.noreply.github.com>
2024-05-01 20:31:36 +0900

    BEにリクエスト送信

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,46 +1,45 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/Prisma";
 import bcrypt from "bcrypt";
 
 const handler = NextAuth({
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET || "secret_for_local_development", // ローカル開発用のデフォルト値
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       name: "Credentials",
       credentials: {
         email: { label: "Username", type: "text" },
-        password: { label: "Password", type: "password" },
+        password: { label: "Password", type: "password" }
       },
       async authorize(credentials) {
-        if (!credentials) return null;  // credentials が undefined の場合は早期に null を返す
+        if (!credentials) return null;
         const { email, password } = credentials;
         try {
           const user = await prisma.user.findUnique({
-            where: { email },
+            where: { email }
           });
-          // パスワードが null ではないことを確認し、bcrypt.compare を実行
           if (user && user.cryptedPassword && await bcrypt.compare(password, user.cryptedPassword)) {
-            return user;  // パスワードが一致した場合、ユーザー情報を返す
+            return user;
           }
         } catch (error) {
           console.error('Authentication error:', error);
           return null;
         }
-        return null;  // ユーザーが見つからないか、パスワードが一致しない場合はnullを返す
+        return null;
       },
     }),
     GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID ?? "",
-      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
+      clientId: process.env.GITHUB_CLIENT_ID || "default_github_client_id",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "default_github_client_secret"
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      clientId: process.env.GOOGLE_CLIENT_ID as string, // 'as string' で TypeScript に string 型であることを強制
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     }),
   ],
   pages: {
@@ -50,6 +49,22 @@ const handler = NextAuth({
     strategy: "jwt",
   },
   callbacks: {
+    async signIn({ user, account, profile }) {
+      if (account && account.provider === "google") {
+        const res = await fetch("https://vimemo.fly.dev/api/auth", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            email: user.email,
+            token: account.access_token
+          })
+        });
+        return res.ok;
+      }
+      return true;
+    },
     async redirect({ url, baseUrl }) {
       return url.startsWith(baseUrl) ? url : baseUrl;
     },
