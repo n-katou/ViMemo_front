ab551e7b033705d1ee0f27e773e35222dbb379e8
n-katou <102382177+n-katou@users.noreply.github.com>
2024-05-05 21:01:13 +0900

    prisma削除

--- app/api/signIn/route.ts
+++ app/api/signIn/route.ts
@@ -1,29 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/Prisma";
 import bcrypt from "bcryptjs";
 
-export async function POST(req: NextRequest) {
-  const data = await req.json();
-  const { email, password } = data;
+// export async function POST(req: NextRequest) {
+//   const data = await req.json();
+//   const { email, password } = data;
 
-  try {
-    const user = await prisma.user.findUnique({
-      where: { email: email },
-    });
-    if (!user || !password) throw new Error("User not found");
-    if (user?.cryptedPassword) {  // 'password' を 'cryptedPassword' に修正
-      const isCorrectPassword = await bcrypt.compare(password, user.cryptedPassword);  // ここも修正
-      if (!isCorrectPassword) {
-        throw new Error("Incorrect password");
-      }
-    }
-  } catch {
-    return new NextResponse(
-      JSON.stringify({ errors: "メールアドレスかパスワードが違います" }),
-      { status: 400 }
-    );
-  }
-  return new NextResponse(JSON.stringify({ message: "Success" }), {
-    status: 201,
-  });
-}
+//   try {
+//     const user = await prisma.user.findUnique({
+//       where: { email: email },
+//     });
+//     if (!user || !password) throw new Error("User not found");
+//     if (user?.cryptedPassword) {  // 'password' を 'cryptedPassword' に修正
+//       const isCorrectPassword = await bcrypt.compare(password, user.cryptedPassword);  // ここも修正
+//       if (!isCorrectPassword) {
+//         throw new Error("Incorrect password");
+//       }
+//     }
+//   } catch {
+//     return new NextResponse(
+//       JSON.stringify({ errors: "メールアドレスかパスワードが違います" }),
+//       { status: 400 }
+//     );
+//   }
+//   return new NextResponse(JSON.stringify({ message: "Success" }), {
+//     status: 201,
+//   });
+// }
--- app/api/signUp/route.ts
+++ app/api/signUp/route.ts
@@ -1,35 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/Prisma";
 import bcrypt from "bcryptjs";
 import { validationRegistSchema } from "@/src/validationSchema";
 
-export async function POST(req: NextRequest) {
-  const data = await req.json();
-  const { email, password } = data;
+// export async function POST(req: NextRequest) {
+//   const data = await req.json();
+//   const { email, password } = data;
 
-  // メールアドレス重複確認とバリデーションを同時に行う
-  const [user, validationResult] = await Promise.all([
-    prisma.user.findFirst({ where: { email } }),
-    validationRegistSchema.safeParseAsync(data)
-  ]);
+//   // メールアドレス重複確認とバリデーションを同時に行う
+//   const [user, validationResult] = await Promise.all([
+//     prisma.user.findFirst({ where: { email } }),
+//     validationRegistSchema.safeParseAsync(data)
+//   ]);
 
-  let errors = validationResult.success ? {} : validationResult.error.flatten().fieldErrors;
-  if (user) {
-    errors.email = [...(errors.email || []), "このメールアドレスは既に使用されています"];
-  }
+//   let errors = validationResult.success ? {} : validationResult.error.flatten().fieldErrors;
+//   if (user) {
+//     errors.email = [...(errors.email || []), "このメールアドレスは既に使用されています"];
+//   }
 
-  if (Object.keys(errors).length > 0) {
-    return new NextResponse(JSON.stringify({ errors }), { status: 400 });
-  }
+//   if (Object.keys(errors).length > 0) {
+//     return new NextResponse(JSON.stringify({ errors }), { status: 400 });
+//   }
 
-  // パスワードをハッシュ化してユーザーを作成
-  const hashedPassword = await bcrypt.hash(password, 10);
-  await prisma.user.create({
-    data: {
-      email: email,
-      cryptedPassword: hashedPassword,  // 'password' を 'cryptedPassword' に変更
-    },
-  });
+//   // パスワードをハッシュ化してユーザーを作成
+//   const hashedPassword = await bcrypt.hash(password, 10);
+//   await prisma.user.create({
+//     data: {
+//       email: email,
+//       cryptedPassword: hashedPassword,  // 'password' を 'cryptedPassword' に変更
+//     },
+//   });
 
-  return new NextResponse(JSON.stringify({ message: "Success" }), { status: 201 });
-}
+//   return new NextResponse(JSON.stringify({ message: "Success" }), { status: 201 });
+// }
