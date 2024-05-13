71d43042a63e8a9bb3e516695074685936d9be3b
n-katou <102382177+n-katou@users.noreply.github.com>
2024-05-13 18:34:43 +0900

    jwtに対応

--- pages/youtube_videos.tsx
+++ pages/youtube_videos.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import { YoutubeVideo } from '../types/youtubeVideo';
 import useFirebaseAuth from '../hooks/useFirebaseAuth';
+import jwtDecode from 'jwt-decode';
 
 // YouTube動画をフェッチする非同期関数
 async function fetchYoutubeVideos(page = 1) {
@@ -44,9 +45,8 @@ async function fetchYoutubeVideos(page = 1) {
 }
 
 const YoutubeVideosPage = () => {
-  const router = useRouter(); // ルーターを取得
-  const { currentUser } = useFirebaseAuth();
-  // フェッチした動画データとページネーション情報、エラー状態を管理
+  const router = useRouter();
+  const { currentUser, setCurrentUser } = useFirebaseAuth();
   const [youtubeVideos, setYoutubeVideos] = useState<YoutubeVideo[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [pagination, setPagination] = useState<{
@@ -61,6 +61,34 @@ const YoutubeVideosPage = () => {
     prev_page: 0, // 前ページがあるかどうか
   });
 
+  const jwtDecode = require('jwt-decode').default;
+  useEffect(() => {
+    const token = sessionStorage.getItem('token');
+    if (token) {
+      try {
+        const decodedUser = jwtDecode(token);
+        setCurrentUser(decodedUser);
+      } catch (error) {
+        console.error('Error decoding token:', error);
+      }
+    } else {
+      router.push('/login');
+    }
+
+    const fetchData = async () => {
+      const result = await fetchYoutubeVideos(pagination.current_page);
+      if (result) {
+        setYoutubeVideos(result.videos);
+        setPagination(result.pagination);
+        setError(null);
+      } else {
+        setError('YouTube動画を取得できませんでした');
+      }
+    };
+    fetchData(); // データをフェッチ
+  }, [pagination.current_page, currentUser, router]); // 現在のページが変わるたびにフェッチ
+
+
   const handleTitleClick = async (id: number) => {
     console.log("遷移前のID:", id); // 遷移前のIDをログに出力
     const cleanUrl = `/youtube_videos/${id}`;
@@ -68,33 +96,12 @@ const YoutubeVideosPage = () => {
     console.log("遷移後のURL:", cleanUrl); // 遷移後のURLをログに出力
   };
 
-  // コンポーネントがマウントされたらデータをフェッチ
-  useEffect(() => {
-    if (!currentUser) {
-      router.push('/login'); // ユーザーがログインしていない場合、ログインページにリダイレクト
-      return;
-    }
-    const fetchData = async () => {
-      const result = await fetchYoutubeVideos(pagination.current_page); // 現在のページをフェッチ
 
-      if (result) {
-        setYoutubeVideos(result.videos); // 動画データを更新
-        setPagination(result.pagination); // ページネーション情報を更新
-        setError(null); // エラーメッセージをクリア
-      } else {
-        setError('YouTube動画を取得できませんでした'); // エラー時
-      }
-    };
-
-    fetchData(); // データをフェッチ
-  }, [pagination.current_page, currentUser, router]); // 現在のページが変わるたびにフェッチ
-
-  // エラーが発生した場合
   if (error) {
     return (
       <div className="container">
         <h1>YouTube一覧</h1>
-        <p>{error}</p> {/* エラーメッセージを表示 */}
+        <p>{error}</p>
       </div>
     );
   }
@@ -106,36 +113,24 @@ const YoutubeVideosPage = () => {
         <>
           {youtubeVideos.map((video) => (
             <div key={video.id} className="mb-6 text-left w-full">
-              <h2 onClick={() => handleTitleClick(video.id)} style={{ cursor: 'pointer' }}>
+              <h2 onClick={() => handleTitleClick(video.id)}>
                 {video.title}
               </h2>
-              <div className="video-wrapper">
-                <iframe
-                  className="aspect-video"
-                  src={`https://www.youtube.com/embed/${video.youtube_id}`}
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                />
-              </div>
+              <iframe
+                src={`https://www.youtube.com/embed/${video.youtube_id}`}
+                frameBorder="0"
+                allowFullScreen
+              />
               <p>公開日: {new Date(video.published_at).toLocaleDateString()}</p>
               <p>動画時間: {video.duration}分</p>
             </div>
           ))}
-
-          {/* ページネーション */}
           <div className="pagination">
-            {pagination.prev_page && (
-              <button onClick={() => setPagination({ ...pagination, current_page: pagination.prev_page })}>前ページ</button>
-            )}
-            {pagination.next_page && (
-              <button onClick={() => setPagination({ ...pagination, current_page: pagination.next_page })}>次ページ</button>
-            )}
+            {pagination.prev_page && <button onClick={() => setPagination({ ...pagination, current_page: pagination.prev_page })}>前ページ</button>}
+            {pagination.next_page && <button onClick={() => setPagination({ ...pagination, current_page: pagination.next_page })}>次ページ</button>}
           </div>
         </>
-      ) : (
-        <p>動画がありません。</p>
-      )}
+      ) : <p>動画がありません。</p>}
     </div>
   );
 };
