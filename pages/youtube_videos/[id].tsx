c2e9f81f0bd03ac955847831516700680f0dc978
n-katou <102382177+n-katou@users.noreply.github.com>
2024-05-14 18:06:57 +0900

    youtube showページ表示

--- pages/youtube_videos/[id].tsx
+++ pages/youtube_videos/[id].tsx
@@ -1,14 +1,21 @@
 "use client";
-
 import React, { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 import { YoutubeVideo } from '../../types/youtubeVideo';
+import useFirebaseAuth from '../../hooks/useFirebaseAuth';
+import NoteForm from '../../components/NoteForm';
+import axios from 'axios';
 
-async function fetchYoutubeVideo(id: number) {
+async function fetchYoutubeVideo(id: number, token?: string) {
+  const headers: { [key: string]: string } = {
+    'Accept': 'application/json'
+  };
+  if (token) {
+    headers['Authorization'] = `Bearer ${token}`;
+  }
+
   const res = await fetch(`https://vimemo.fly.dev/api/v1/youtube_videos/${id}`, {
-    headers: {
-      'Accept': 'application/json',
-    },
+    headers: headers,
   });
 
   if (!res.ok) {
@@ -16,43 +23,88 @@ async function fetchYoutubeVideo(id: number) {
   }
 
   const data = await res.json();
-  console.log(data);  // レスポンスデータのログ出力
   return data;
 }
 
 const YoutubeVideoShowPage = () => {
   const [video, setVideo] = useState<YoutubeVideo | null>(null);
   const pathname = usePathname();
+  const { currentUser } = useFirebaseAuth();
+
+  const [notes, setNotes] = useState<any[]>([]);
+  const addNote = async (newNoteContent: string): Promise<void> => {
+    if (!currentUser || !video) {
+      console.error('User or video is not defined');
+      return;
+    }
+
+    try {
+      const token = await currentUser.getIdToken();
+      const response = await axios.post(
+        `https://vimemo.fly.dev/api/v1/youtube_videos/${video.id}/notes`,
+        { content: newNoteContent },
+        {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+          }
+        }
+      );
+
+      setNotes(prevNotes => [...prevNotes, response.data]);
+    } catch (error) {
+      console.error('Failed to add note:', error);
+    }
+  };
 
   useEffect(() => {
     const pathSegments = pathname.split('/');
     const videoId = parseInt(pathSegments[pathSegments.length - 1], 10);
 
     if (!isNaN(videoId)) {
-      fetchYoutubeVideo(videoId)
-        .then(videoData => {
-          setVideo(videoData.youtube_video);  // 期待するキーに注意
-        })
-        .catch(error => console.error('Error loading the video:', error));
+      const fetchVideo = async () => {
+        let token;
+        if (currentUser) {
+          token = await currentUser.getIdToken();
+        }
+        fetchYoutubeVideo(videoId, token)
+          .then(videoData => {
+            setVideo(videoData.youtube_video);
+            console.log('Video data loaded', videoData);  // ビデオデータがロードされた後にログ出力
+          })
+          .catch(error => console.error('Error loading the video:', error));
+      };
+      fetchVideo();
     }
-  }, [pathname]);
+  }, [pathname, currentUser]);
 
   if (!video) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
-      <h1>{video.title || "タイトル不明"}</h1>
-      <iframe
-        src={`https://www.youtube.com/embed/${video.youtube_id}`}
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
-      <p>公開日: {new Date(video.published_at).toLocaleDateString()}</p>
-      <p>動画時間: {video.duration}分</p>
-    </div>
+    <>
+      <div>
+        <h1>{video.title || "タイトル不明"}</h1>
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${video.youtube_id}`}
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen />
+        <p>公開日: {new Date(video.published_at).toLocaleDateString()}</p>
+        <p>動画時間: {video.duration}分</p>
+      </div>
+      <div>
+        {currentUser && <NoteForm addNote={addNote} />}
+        <div>
+          {notes.map((note, index) => (
+            <p key={index}>{note.content}</p>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
