3222d51d7fe7802845cba6fbd99684996d9e1740
n-katou <102382177+n-katou@users.noreply.github.com>
2024-05-22 02:20:10 +0900

    メモソート

--- pages/my_notes.tsx
+++ pages/my_notes.tsx
@@ -4,6 +4,7 @@ import { useAuth } from '../context/AuthContext';
 import LoadingSpinner from '../components/LoadingSpinner';
 import NoteCard from '../components/Note/NoteCard';
 import { Note } from '../types/note';
+import { FaSortAmountDown, FaSortAmountUp } from 'react-icons/fa';
 
 interface NoteWithVideoTitle extends Note {
   video_title?: string;
@@ -14,6 +15,7 @@ const MyNotesPage: React.FC = () => {
   const [notes, setNotes] = useState<NoteWithVideoTitle[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('desc'); // ソートの状態を管理
 
   useEffect(() => {
     if (!currentUser) {
@@ -65,15 +67,36 @@ const MyNotesPage: React.FC = () => {
     fetchNotes();
   }, [currentUser, jwtToken]);
 
+  const sortedNotes = notes.sort((a, b) => {
+    if (sortOrder === 'asc') {
+      return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
+    } else {
+      return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+    }
+  });
+
+  const toggleSortOrder = () => {
+    setSortOrder((prevSortOrder) => (prevSortOrder === 'asc' ? 'desc' : 'asc'));
+  };
+
   if (loading) return <LoadingSpinner loading={loading} />;
   if (error) return <p>{error}</p>;
 
   return (
     <div className="container mx-auto py-8">
-      <h1 className="text-2xl font-bold mb-4">自分のメモ</h1>
-      {notes.length > 0 ? (
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl font-bold">自分のメモ</h1>
+        <button
+          onClick={toggleSortOrder}
+          className="bg-blue-500 text-white px-4 py-2 rounded flex items-center gap-2"
+        >
+          {sortOrder === 'asc' ? <FaSortAmountUp /> : <FaSortAmountDown />}
+          {sortOrder === 'asc' ? '古い順' : '新しい順'}
+        </button>
+      </div>
+      {sortedNotes.length > 0 ? (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {notes.map((note) => (
+          {sortedNotes.map((note) => (
             <NoteCard
               key={note.id}
               videoTitle={note.video_title || 'タイトルなし'}
