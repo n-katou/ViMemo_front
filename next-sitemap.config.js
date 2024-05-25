db55c783a457c869e66c6ea68983f9828920995d
n-katou <102382177+n-katou@users.noreply.github.com>
2024-05-25 15:52:59 +0900

    サイトマップ修正,package.json修正

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,7 @@
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://vimemo.vercel.app',
   generateRobotsTxt: true, // (optional) Set to true if you want to generate robots.txt
-  // other options
+  sitemapSize: 5000,
+  outDir: 'out', // 生成されたサイトマップの出力先ディレクトリ
+  generateIndexSitemap: true, // インデックスサイトマップを生成するかどうか
 };
