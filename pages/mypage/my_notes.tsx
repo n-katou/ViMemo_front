c2b1c93791324b53d28dba2b1638ae59ecf1371f
n-katou <102382177+n-katou@users.noreply.github.com>
2024-05-26 22:31:35 +0900

    ノートソート修正

--- pages/mypage/my_notes.tsx
+++ pages/mypage/my_notes.tsx
@@ -4,7 +4,6 @@ import { useAuth } from '../../context/AuthContext';
 import LoadingSpinner from '../../components/LoadingSpinner';
 import NoteCard from '../../components/Note/NoteCard';
 import { Note } from '../../types/note';
-import { FaSortAmountDown, FaSortAmountUp } from 'react-icons/fa';
 import PaginationComponent from '../../components/Pagination';  // 共通コンポーネントをインポート
 
 interface NoteWithVideoTitle extends Note {
@@ -16,7 +15,7 @@ const MyNotesPage: React.FC = () => {
   const [notes, setNotes] = useState<NoteWithVideoTitle[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('desc');
+  const [sortOption, setSortOption] = useState<string>('created_at_desc');  // ソートオプションを追加
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
 
@@ -27,9 +26,9 @@ const MyNotesPage: React.FC = () => {
       return;
     }
 
-    const fetchNotes = async (page: number) => {
+    const fetchNotes = async (page: number, sort: string) => {
       try {
-        const userNotesUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/notes?filter=my_notes&page=${page}`;
+        const userNotesUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/notes?filter=my_notes&page=${page}&sort=${sort}`;
         console.log(`Request URL for user notes: ${userNotesUrl}`);
 
         const res = await axios.get(userNotesUrl, {
@@ -68,8 +67,8 @@ const MyNotesPage: React.FC = () => {
       }
     };
 
-    fetchNotes(page);
-  }, [currentUser, jwtToken, page]);
+    fetchNotes(page, sortOption);
+  }, [currentUser, jwtToken, page, sortOption]);  // sortOptionを依存配列に追加
 
   const handleDeleteNote = async (noteId: number) => {
     try {
@@ -85,16 +84,9 @@ const MyNotesPage: React.FC = () => {
     }
   };
 
-  const sortedNotes = notes.sort((a, b) => {
-    if (sortOrder === 'asc') {
-      return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
-    } else {
-      return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
-    }
-  });
-
-  const toggleSortOrder = () => {
-    setSortOrder((prevSortOrder) => (prevSortOrder === 'asc' ? 'desc' : 'asc'));
+  const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortOption(event.target.value);
+    setPage(1);  // ソートオプションが変更された場合、ページをリセット
   };
 
   const handlePageChange = (_event: React.ChangeEvent<unknown>, newPage: number) => {
@@ -111,17 +103,18 @@ const MyNotesPage: React.FC = () => {
     <div className="container mx-auto py-8">
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-2xl font-bold">自分のメモ</h1>
-        <button
-          onClick={toggleSortOrder}
-          className="bg-blue-500 text-white px-4 py-2 rounded flex items-center gap-2"
+        <select
+          value={sortOption}
+          onChange={handleSortChange}
+          className="form-select form-select-lg text-white bg-gray-800 border-gray-600"
         >
-          {sortOrder === 'asc' ? <FaSortAmountUp /> : <FaSortAmountDown />}
-          {sortOrder === 'asc' ? '古い順' : '新しい順'}
-        </button>
+          <option value="created_at_desc">デフォルト（新しい順）</option>
+          <option value="created_at_asc">古い順</option>
+        </select>
       </div>
-      {sortedNotes.length > 0 ? (
+      {notes.length > 0 ? (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {sortedNotes.map((note) => (
+          {notes.map((note) => (
             <NoteCard
               key={note.id}
               videoTitle={note.video_title || 'タイトルなし'}
