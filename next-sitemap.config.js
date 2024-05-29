3f39ebb1242eecd43773a30d6ea698fc9d37cced
n-katou <102382177+n-katou@users.noreply.github.com>
2024-05-29 19:27:11 +0900

    sitemapの更新

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,6 @@ module.exports = {
   siteUrl: 'https://vi-memo.com',
   generateRobotsTxt: true, // (optional) Set to true if you want to generate robots.txt
   sitemapSize: 5000,
-  outDir: 'out', // 生成されたサイトマップの出力先ディレクトリ
+  outDir: 'public', // 生成されたサイトマップの出力先ディレクトリ
   generateIndexSitemap: true, // インデックスサイトマップを生成するかどうか
 };
