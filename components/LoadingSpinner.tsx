65095fa363d377961c883e5b302b36cfcc778b6d
TISNT\tikK90316 <kato.naoto@tis.co.jp>
2024-06-12 10:23:57 +0900

    スピナーデザイン変更

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -1,20 +1,24 @@
-// components/LoadingSpinner.tsx
 import React from 'react';
-import ClipLoader from 'react-spinners/ClipLoader';
+import { useTheme } from 'next-themes';
 
 interface LoadingSpinnerProps {
   loading: boolean;
-  size?: number;
-  color?: string;
+  size?: number; // サイズは画像の表示に合わせて調整できます
+  bgColor?: string; // 背景色をカスタマイズできるようにする
 }
 
-const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({ loading, size = 50, color = "#123abc" }) => {
+const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({ loading, size = 100, bgColor = 'rgba(0, 0, 0, 0.75)' }) => {
+  const { resolvedTheme } = useTheme(); // next-themesからテーマを取得
+  const isDarkMode = resolvedTheme === 'dark';
+
+  const spinnerImage = isDarkMode ? '/black_spinner.gif' : '/white_spinner.gif';
+
   if (!loading) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
+    <div className="fixed inset-0 flex items-center justify-center" style={{ backgroundColor: bgColor, zIndex: 50 }}>
       <div className="text-center">
-        <ClipLoader size={size} color={color} loading={loading} />
+        <img src={spinnerImage} alt="Loading..." style={{ width: size, height: size, maxWidth: '100%', maxHeight: '100%' }} />
         <p className="mt-4 text-white text-lg font-semibold">Loading...</p>
       </div>
     </div>
