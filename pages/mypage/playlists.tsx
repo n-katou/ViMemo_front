aaaaf56c72654fbf1b84fe8fc765f6bc9929e04a
n-katou <102382177+n-katou@users.noreply.github.com>
2025-04-04 23:09:05 +0900

    サイドボタンが動くように調整

--- pages/mypage/playlists.tsx
+++ pages/mypage/playlists.tsx
@@ -75,12 +75,19 @@ const PlaylistPage = () => {
     <DndProvider backend={HTML5Backend}>
       <main className="relative h-screen w-full">
         {/* 開閉ボタン */}
-        <div className="fixed top-4 right-4 z-50 md:left-64 top-[100px] ">
+        <div
+          className={`fixed top-[100px] z-50 transition-all duration-300 ${showSidebar ? "right-4 md:left-64" : "left-4"
+            }`}
+        >
           <button
             className="p-2 rounded-full bg-blue-600 text-white shadow-md hover:bg-blue-700 transition"
             onClick={() => setShowSidebar(!showSidebar)}
           >
-            {showSidebar ? <ChevronLeftIcon fontSize="small" /> : <ChevronRightIcon fontSize="small" />}
+            {showSidebar ? (
+              <ChevronLeftIcon fontSize="small" />
+            ) : (
+              <ChevronRightIcon fontSize="small" />
+            )}
           </button>
         </div>
         <div className="flex h-full overflow-hidden">
