c776e6f3c1e3f5fe79f20eb5d94c22d5199aaea7
n-katou <102382177+n-katou@users.noreply.github.com>
2025-04-04 23:56:36 +0900

    並び替え時ボタンの位置調整

--- components/Mypage/playlists/PlaylistVideos.tsx
+++ components/Mypage/playlists/PlaylistVideos.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import FavoriteVideoCard from "../favorite_videos/FavoriteVideoCard";
 import { YoutubeVideo } from "../../../types/youtubeVideo"; // ← これに合わせて定義
 
@@ -23,6 +23,8 @@ const PlaylistVideos: React.FC<Props> = ({
   jwtToken,
   playlistId,
 }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
   const moveVideo = (dragIndex: number, hoverIndex: number) => {
     const updated = [...playlistItems];
     const [moved] = updated.splice(dragIndex, 1);
@@ -45,7 +47,9 @@ const PlaylistVideos: React.FC<Props> = ({
         }
       );
       if (!res.ok) throw new Error("Failed to save order");
-      console.log("並び順を保存しました");
+
+      setShowTooltip(true);
+      setTimeout(() => setShowTooltip(false), 2000);
     } catch (err) {
       console.error("並び順保存に失敗:", err);
     }
@@ -53,14 +57,26 @@ const PlaylistVideos: React.FC<Props> = ({
 
   return (
     <div className="w-full px-6 py-8 pt-16 pb-24">
-      {playlistId && (
-        <button
-          onClick={handleSaveOrder}
-          className="mb-4 px-4 py-2 bg-blue-500 text-white rounded-md"
-        >
-          並び順を保存する
-        </button>
+      {/* Tooltip表示 */}
+      {showTooltip && (
+        <div className="fixed top-20 sm:top-24 md:top-28 left-1/2 -translate-x-1/2 z-50 px-3 py-2 bg-green-500 text-white text-sm rounded shadow-md animate-fadeIn">
+          保存しました。
+        </div>
       )}
+      {playlistId && (
+        <>
+          <div className="relative inline-block mb-4">
+            <button
+              onClick={handleSaveOrder}
+              className="px-4 py-2 bg-blue-500 text-white rounded-md"
+            >
+              並び順を保存する
+            </button>
+
+          </div>
+        </>
+      )}
+
       <div className="grid grid-cols-1 sm:grid-cols-4 gap-4">
         {playlistItems.map((item, index) => (
           <FavoriteVideoCard
